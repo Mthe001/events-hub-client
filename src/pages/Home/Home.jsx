71431931057f6c2768af0321737a417cbf6d65e1
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-02-24 11:25:04 +0600

    💫 Feat Home
    home init of this project

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async';
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Home | Local Event Hub 240</title>
+            </Helmet> 
+            
+            <section className="flex items-center justify-center min-h-screen">
+               <h1>Hello Im home</h1>
+            </section>
+
+            
+
+    
+            
+        </div>
+    );
+};
+
+export default Home;
