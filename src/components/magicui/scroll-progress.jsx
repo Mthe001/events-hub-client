fb36827763dcc39a0e42c9f625159fd588fc4319
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-03-04 06:20:47 +0600

    💄 Scroll-Progress
    scrolling progress color changed

--- src/components/magicui/scroll-progress.jsx
+++ src/components/magicui/scroll-progress.jsx
@@ -17,7 +17,7 @@ export const ScrollProgress = React.forwardRef(({ className, ...props }, ref) =>
     <motion.div
       ref={ref}
       className={cn(
-        "fixed top-0 right-0 z-[1000] w-1 h-full origin-top bg-gradient-to-b from-[#808080] via-[#87CEEB] to-[#0000FF]",
+        "fixed top-0 right-0 z-[1000] w-1 h-full origin-top bg-gradient-to-b from-[#808080] via-[#87CEEB]  to-[#000000]",
         className
       )}
       style={{
