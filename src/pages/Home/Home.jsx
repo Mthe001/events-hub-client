9dce0569257732fe4a4744422c9338b34903c4ff
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-03-06 16:10:56 +0600

    💫 Marquee
    marquee init for most trusted partners in home / page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 import SwiperSlider from './Slider';
+import { MarqueeDemo } from '@/components/Marquee3d';
+import { TextAnimate } from '@/components/magicui/text-animate';
+
 
 
 
@@ -12,7 +15,17 @@ const Home = () => {
 
             <section className='w-[95%] mx-auto'>
                 <SwiperSlider />
-            </section>
+            </section> 
+
+            <section className='w-[95%] my-1 mx-auto'>
+               <div className='flex items-center flex-col my-5'>
+                   
+                    <TextAnimate animation='blurInUp' by='character' className='lg:text-6xl md:text-4xl text-xl text-stone-400 font-semibold  mb-4'>Most trusted Partners</TextAnimate>
+                    <div className='w-[75%] mx-auto'>
+                        <MarqueeDemo />
+                    </div>
+               </div>
+            </section> 
 
             <section className="flex items-center justify-center min-h-screen">
                <h1>Hello Im home</h1>
